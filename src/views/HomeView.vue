468aa68dfd45f423a39e823fbec77d8d39c59993
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-04-06 16:35:14 +0530

    added blog post and nav

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,43 @@
 <template>
-  <Navbar/>
+  <Navbar />
+
+  <div class="py-8 mx-auto max-w-screen-xl text-center lg:py-12 lg:px-6">
+    <div class="grid gap-8 lg:gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-end">
+
+      <template v-for="post in posts" :key="post">
+        <div class="relative rounded-lg overflow-hidden h-max">
+          <div class="rounded-lg relative">
+            <!-- <img :src="post.feature_image" :alt="post.title" /> -->
+            <img src="/assets/create-a-realistic-painting-or-digital-artwork-that-captures-the-emotional-depth-of-the-laughter-ma-118286241.png" alt="img">
+          </div>
+          <div class="bg-black bg-opacity-10 flex flex-col items-center justify-end pb-4">
+            <!-- <h1 class="text-white text-xl font-bold">{{ post.title }}</h1> -->
+            <p class="text-gray-100 text-sm">{{ post.title }}</p>
+          </div>
+        </div>
+      </template>
+
+    </div>
+  </div>
 
 </template>
 <script>
 import Navbar from '@/components/Navbar.vue';
-export default{
+import { getPosts } from '@/API/index.js';
+
+export default {
   name: 'HomeView',
   components: {
     Navbar,
+  },
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async mounted() {
+    this.posts = await getPosts();
   }
 }
-  
+
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ export default {
   ],
   theme: {
     extend: {
+      colors: {
+        bgPrimary: '#FBFBFF', // background primary
+        primary: '#111928', // text 
+        secondary: '#6C2BD9', // selection / hover
+        dark: '6C2BD9', // Darkmod
+      }
     }
   },
   plugins: [
