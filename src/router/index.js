18731ae62961f431e208445c21285cda145333e3
Badhusha <Badhusha.shaji22@example.com>
2024-04-06 19:56:20 +0530

    Blogscreen

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,9 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/blog',
+      path: '/:slug',
       name: 'blog',
       component: Blogview
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
