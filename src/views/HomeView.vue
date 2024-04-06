5d3f2f6548582c354b97e8a0d68ff7e9ef8c2349
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-04-06 17:14:39 +0530

    added

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,18 @@
 <template>
   <Navbar />
 
-  <div class="py-8 mx-auto max-w-screen-xl text-center lg:py-12 lg:px-6">
+  <div class="py-8 mx-auto max-w-screen-xl mt-8 text-center lg:py-12 lg:px-6">
     <div class="grid gap-8 lg:gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-end">
 
       <template v-for="post in posts" :key="post">
-        <div class="relative rounded-lg overflow-hidden h-max">
-          <div class="rounded-lg relative">
+        <div class="relative rounded-lg overflow-hidden h-80">
+          <div class="rounded-lg absolute">
             <!-- <img :src="post.feature_image" :alt="post.title" /> -->
             <img src="/assets/create-a-realistic-painting-or-digital-artwork-that-captures-the-emotional-depth-of-the-laughter-ma-118286241.png" alt="img">
           </div>
-          <div class="bg-black bg-opacity-10 flex flex-col items-center justify-end pb-4">
-            <!-- <h1 class="text-white text-xl font-bold">{{ post.title }}</h1> -->
-            <p class="text-gray-100 text-sm">{{ post.title }}</p>
+          <div class="bg-black bg-opacity-10 flex flex-col items-center justify-end pb-4 relative">
+            <h1 class="text-white text-xl font-bold">{{ post.title }}</h1>
+            <p class="text-gray-100 text-sm">{{ post.slug }}</p>
           </div>
         </div>
       </template>
